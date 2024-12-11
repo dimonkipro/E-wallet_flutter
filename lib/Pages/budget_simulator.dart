e212c77fe6c0c0c9333bea0e7e41e53d9bf92e45
baboucha <dimonkipro@gmail.com>
2024-12-11 19:01:13 +0100

    Add message with colors and scrollView to budget_simulator

--- lib/Pages/budget_simulator.dart
+++ lib/Pages/budget_simulator.dart
@@ -14,6 +14,11 @@ class _BudgetSimulatorState extends State<BudgetSimulator> {
   final List<Map<String, dynamic>> _revenues = [];
   final List<Map<String, dynamic>> _expenses = [];
 
+// GETTER FOR THE FUNCTION CALCULATE_TOTAL WITH PARAMETERS
+  double get totalRevenues => _calculateTotal(_revenues);
+  double get totalExpenses => _calculateTotal(_expenses);
+  double get balance => totalRevenues - totalExpenses;
+
 // FUNCTION TO REFRESH THE PAGE
   void _refreshPage() {
     setState(() {
@@ -43,43 +48,40 @@ class _BudgetSimulatorState extends State<BudgetSimulator> {
     });
   }
 
-// ITERATE THROUGH THE LISTS AND CALCULATE TOTAL AMOUNTS
+
+// FUNCTION TO ITERATE THROUGH THE LISTS AND CALCULATE TOTAL AMOUNTS
   double _calculateTotal(List<Map<String, dynamic>> items) {
     double total = 0;
     for (var item in items) {
 
-// RETRIEVE THE AMOUNT
+      // RETRIEVE THE AMOUNT
       final amountText = item['amount'].text;
       if (amountText.isNotEmpty && double.tryParse(amountText) != null) {
 
-// ADD TO TOTAL
+      // ADD TO TOTAL
         total += double.parse(amountText);
       }
     }
     return total;
   }
 
-// GETTER FOR THE FUNCTION CALCULATE_TOTAL WITH PARAMETERS
-  double get totalRevenues => _calculateTotal(_revenues);
-  double get totalExpenses => _calculateTotal(_expenses);
-  double get balance => totalRevenues - totalExpenses;
 
 
 // FUNCTION FOR ALERT WITH ALL DETAILS INSERTED AND TOTALS
   void _showSummaryDialog(BuildContext context) {
 
-// INITIALIZE THE ALERT BODY
+    // INITIALIZE THE ALERT BODY
     String summary = 'Revenus :\n';
 
     for (var revenue in _revenues) {
-// RETRIEVE THE TYPE
+    // RETRIEVE THE TYPE
       final type = revenue['type'] ?? 'Inconnu';
 
-// RETRIEVE THE AMOUNT
+    // RETRIEVE THE AMOUNT
       final amount =
           revenue['amount'].text.isNotEmpty ? revenue['amount'].text : '0';
 
-// ADD A LINE CONTAIN THE DETAILS TO THE REVENUES DETAILS
+    // ADD A LINE CONTAIN THE DETAILS TO THE REVENUES DETAILS
       summary += '- $type : $amount TND\n';
     }
 
@@ -89,22 +91,43 @@ class _BudgetSimulatorState extends State<BudgetSimulator> {
       final amount =
           expense['amount'].text.isNotEmpty ? expense['amount'].text : '0';
 
-// ADD A LINE CONTAIN THE DETAILS TO THE EXPENSES DETAILS
+    // ADD A LINE CONTAIN THE DETAILS TO THE EXPENSES DETAILS
       summary += '- $type : $amount TND\n';
     }
 
-// ADD A LINE CONTAIN THE DETAILS TO THE TOTALS
+    // ADD A LINE CONTAIN THE DETAILS TO THE TOTALS
     summary +=
         '\nTotal des revenus : ${totalRevenues.toStringAsFixed(2)} TND\n';
     summary += 'Total des dépenses : ${totalExpenses.toStringAsFixed(2)} TND\n';
-    summary += 'Solde restant : ${balance.toStringAsFixed(2)} TND';
 
-// INITIALIZE THE DIALOG WITH HEADER AND CONTENT FROM THE ALERT
+    // INITIALIZE THE DIALOG WITH HEADER AND CONTENT FROM THE ALERT
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
         title: const Text('Résumé'),
-        content: Text(summary),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(summary),
+          const SizedBox(height: 10),
+          Text(
+            'Solde restant : ${balance.toStringAsFixed(2)} TND \n',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: _getBalanceColor(),
+            ),
+          ),
+          Text(
+            _getBalanceMessage(),
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+
+            ),
+          ),
+        ],
+        ),
         actions: [
           TextButton(
             onPressed: () {
@@ -116,6 +139,32 @@ class _BudgetSimulatorState extends State<BudgetSimulator> {
       ),
     );
   }
+
+// FUNCTION TO GET THE BALANCE MESSAGE
+  String _getBalanceMessage() {
+    if (balance > 0) {
+      return "Vous êtes dans le vert ! Vous avez un surplus de ${balance.toStringAsFixed(2)} TND.";
+    } else if (balance < 0) {
+      return "Attention ! Vous êtes dans le rouge avec un déficit de ${(-balance).toStringAsFixed(2)} TND.";
+    } else {
+      return "Votre solde est équilibré.";
+    }
+  }
+
+// FUNCTION TO GET THE COLOR FROM THE BALANCE CONDITION
+  Color _getBalanceColor() {
+    if (balance > 0) {
+      return Colors.green;
+    } else if (balance < 0) {
+      return Colors.red;
+    } else {
+      return Colors.blue;
+    }
+  }
+
+
+// ------------------ END OF FUNCTIONS AND DECLARATION ------------------------
+
 
   @override
   Widget build(BuildContext context) {
@@ -138,189 +187,201 @@ class _BudgetSimulatorState extends State<BudgetSimulator> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
+    // SINGLE_CHILD_SCROLLVIEW TO ALLOW THE USER TO SCROLL
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
 // REVENUES Section
-              const Text(
-                'Revenus',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
+                const Text(
+                  'Revenus',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
 
-// TRANSFORM OF LIST _revenues TO Map ['salaire',..] devient {0: 'salaire',..}
-// ENTRIES RETURNS A COLLECTION OF KEY-VALUE PAIRS
-              ..._revenues.asMap().entries.map((entry) {
+    // TRANSFORM OF LIST _revenues TO Map ['salaire',..] devient {0: 'salaire',..}
+    // ENTRIES RETURNS A COLLECTION OF KEY-VALUE PAIRS
+                ..._revenues.asMap().entries.map((entry) {
 
-// INDEX OF THE ELEMENT
-                int index = entry.key;
+    // INDEX OF THE ELEMENT
+                  int index = entry.key;
 
-// THE ELEMENT ITSELF
-                var revenue = entry.value;
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Row(
-                    children: [
-// REVENUES Type
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Type de revenu',
-                            hintText: 'Salaire, Retraite, Loyer...',
+    // THE ELEMENT ITSELF
+                  var revenue = entry.value;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+    // REVENUES Type
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Type de revenu',
+                              hintText: 'Salaire, Retraite, Loyer...',
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Entrez un type';
+                              }
+                              return null;
+                            },
+                            onChanged: (value) => revenue['type'] = value,
                           ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Entrez un type';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) => revenue['type'] = value,
                         ),
-                      ),
 
-                      const SizedBox(width: 8),
+                        const SizedBox(width: 8),
 
-// REVENUES Amount
-                      Expanded(
-                        child: TextFormField(
-                          controller: revenue['amount'],
-                          decoration: const InputDecoration(
-                            labelText: 'Montant (TND)',
+    // REVENUES Amount
+                        Expanded(
+                          child: TextFormField(
+                            controller: revenue['amount'],
+                            decoration: const InputDecoration(
+                              labelText: 'Montant (TND)',
+                            ),
+                            keyboardType: TextInputType.number,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Entrez une valeur';
+                              }
+                              if (double.tryParse(value) == null) {
+                                return 'Entrez un nombre valide';
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              setState(() {});
+                            },
                           ),
-                          keyboardType: TextInputType.number,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Entrez une valeur';
-                            }
-                            if (double.tryParse(value) == null) {
-                              return 'Entrez un nombre valide';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            setState(() {});
-                          },
                         ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.black26),
-                        onPressed: () => _removeRow(_revenues, index),
-                      ),
-                    ],
-                  ),
-                );
-              }),
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.black26),
+                          onPressed: () => _removeRow(_revenues, index),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
 
-// ADD REVENUES BUTTON
-              ElevatedButton(
-                onPressed: _addRevenueRow,
-                child: const Text('Ajouter un revenu'),
-              ),
+    // ADD REVENUES BUTTON
+                ElevatedButton(
+                  onPressed: _addRevenueRow,
+                  child: const Text('Ajouter un revenu'),
+                ),
 
-              const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
 // EXPENSES Section
-              const Text(
-                'Dépenses',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
+                const Text(
+                  'Dépenses',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
 
-// TRANSFORM OF LIST _expenses TO Map ['transport',...] devient {0: 'transport',...'}
-// ENTRIES RETURNS A COLLECTION OF KEY-VALUE PAIRS
-              ..._expenses.asMap().entries.map((entry) {
-// INDEX OF THE ELEMENT
-                int index = entry.key;
+    // TRANSFORM OF LIST _expenses TO Map ['transport',...] devient {0: 'transport',...'}
+    // ENTRIES RETURNS A COLLECTION OF KEY-VALUE PAIRS
+                ..._expenses.asMap().entries.map((entry) {
+    // INDEX OF THE ELEMENT
+                  int index = entry.key;
 
-// THE ELEMENT ITSELF
-                var expense = entry.value;
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Row(
-                    children: [
-// EXPENSES Type
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                              labelText: 'Type de dépense',
-                              hintText: 'Transport, Internet, Habillement...'),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Entrez un type';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) => expense['type'] = value,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-
-// EXPENSES Amount
-                      Expanded(
-                        child: TextFormField(
-                          controller: expense['amount'],
-                          decoration: const InputDecoration(
-                            labelText: 'Montant (TND)',
+    // THE ELEMENT ITSELF
+                  var expense = entry.value;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+    // EXPENSES Type
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Type de dépense',
+                                hintText: 'Transport, Internet, Habillement...'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Entrez un type';
+                              }
+                              return null;
+                            },
+                            onChanged: (value) => expense['type'] = value,
                           ),
-                          keyboardType: TextInputType.number,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Entrez une valeur';
-                            }
-                            if (double.tryParse(value) == null) {
-                              return 'Entrez un nombre valide';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            setState(() {});
-                          },
                         ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.black26),
-                        onPressed: () => _removeRow(_expenses, index),
-                      ),
-                    ],
-                  ),
-                );
-              }),
+                        const SizedBox(width: 8),
 
-// ADD EXPENSES BUTTON
-              ElevatedButton(
-                onPressed: _addExpenseRow,
-                child: const Text('Ajouter une dépense'),
-              ),
+    // EXPENSES Amount
+                        Expanded(
+                          child: TextFormField(
+                            controller: expense['amount'],
+                            decoration: const InputDecoration(
+                              labelText: 'Montant (TND)',
+                            ),
+                            keyboardType: TextInputType.number,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Entrez une valeur';
+                              }
+                              if (double.tryParse(value) == null) {
+                                return 'Entrez un nombre valide';
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              setState(() {});
+                            },
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.black26),
+                          onPressed: () => _removeRow(_expenses, index),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
 
-              const SizedBox(height: 20),
+    // ADD EXPENSES BUTTON
+                ElevatedButton(
+                  onPressed: _addExpenseRow,
+                  child: const Text('Ajouter une dépense'),
+                ),
+
+                const SizedBox(height: 20),
 
 // TOTALS
-              Text(
-                'Total des revenus : ${totalRevenues.toStringAsFixed(2)} TND',
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Total des dépenses : ${totalExpenses.toStringAsFixed(2)} TND',
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Solde restant : ${balance.toStringAsFixed(2)} TND',
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-
-              const SizedBox(height: 20),
+                Text(
+                  'Total des revenus : ${totalRevenues.toStringAsFixed(2)} TND',
+                  style:
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Total des dépenses : ${totalExpenses.toStringAsFixed(2)} TND',
+                  style:
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Solde restant : ${balance.toStringAsFixed(2)} TND',
+                  style:
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+// BALANCE MESSAGE
+                const SizedBox(height: 20),
+                Text(
+                  _getBalanceMessage(),
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: _getBalanceColor(),
+                  ),
+                ),
+                const SizedBox(height: 20),
 
 // SUBMIT BUTTON
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _showSummaryDialog(context);
-                  }
-                },
-                child: const Text('Soumettre'),
-              ),
-            ],
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _showSummaryDialog(context);
+                    }
+                  },
+                  child: const Text('Soumettre'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
